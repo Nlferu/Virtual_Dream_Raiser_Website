e8f2c91747917b3b6d68443fb4565efde9f63a08
Niferu <wargacki.patryq@gmail.com>
2023-12-09 11:11:42 +0100

    starsCanvas fix and wallet dependencies added

--- components/starBackground.tsx
+++ components/starBackground.tsx
@@ -1,10 +1,7 @@
-"use client"
-
 import React, { useState, useRef, Suspense } from "react"
 import { Canvas, useFrame } from "@react-three/fiber"
 import { Points, PointMaterial, Preload } from "@react-three/drei"
-// @ts-ignore
-import * as random from "maath/random/dist/maath-random.esm"
+import * as random from "maath/random"
 
 const StarBackground = (props: any) => {
     const ref: any = useRef()
